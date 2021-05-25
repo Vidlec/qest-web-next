23469e8be65ea01b1754aa81e347edb200a1d4ce
Vit Machalek <vit.mach61@gmail.com>
2021-05-25 13:26:24 +0200

    feat(contacts) form handeling add states for all the inputs of the form and sending state

--- pages/contacts/index.tsx
+++ pages/contacts/index.tsx
@@ -15,33 +15,64 @@ import {
 
 // TODO: Add Formular and Header
 const Contacts: React.FC = () => {
-    const [textAreaInput, setTextAreaInput] = useState("");
-    const [nameInput, setNameInput] = useState("");
-    const [emailInput, setEmailInput] = useState("");
-    const [isSending, setIsSending] = useState(false);
+    const [textAreaInput, setTextAreaInput] = useState("")
+    const [nameInput, setNameInput] = useState("")
+    const [emailInput, setEmailInput] = useState("")
+    const [isSending, setIsSending] = useState(false)
 
     const { t } = useTranslation();
+
+    const fakeSubmit = () => {
+        return new Promise((res) => {
+            setTimeout(() => {res("")}, 3000)
+        })
+    }
+
+    const handleSubmit = async (e: React.SyntheticEvent) => {
+        e.preventDefault()
+        if( nameInput === "" || emailInput === "" || textAreaInput === "" ) {
+            return
+        }
+        setIsSending(true);
+        try {
+            await fakeSubmit()
+            setIsSending(false);
+        } catch (error) {
+            setIsSending(false);
+        }
+    }
+
+    const testEmail = (email: string) => {
+        const re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        return(re.test(email));
+    }
+
+    const handleEmail = (e: React.ChangeEvent<HTMLInputElement>) => {
+        
+        setEmailInput(e.target.value)
+    }
 
     return (
         <>
             <Footer />
             
             <Container>
-                <FormWrapper>
+                <FormWrapper 
+                    onSubmit={handleSubmit}>
                     <FormLabel>
-                        napište nám
+                        napište nám:
                         <FormTextArea value={textAreaInput} onChange={e => setTextAreaInput(e.target.value)} />    
                     </FormLabel>
                     <FormRow>
                         <FormLabel>
-                            jméno
+                            jméno:
                             <FormInput type="text" value={nameInput} onChange={e => setNameInput(e.target.value)} />
                         </FormLabel>
                         <FormLabel>
-                            email
-                            <FormInput type="email" value={emailInput} onChange={e => setEmailInput(e.target.value)}/>
+                            email:
+                            <FormInput type="text" value={emailInput} onChange={handleEmail}/>
                         </FormLabel>
-                        <FormSubmit type="submit" value="submit" disabled={isSending} />
+                        <FormSubmit type="submit" value="ODESLAT" disabled={isSending} />
                     </FormRow>
                 </FormWrapper>
             </Container>
