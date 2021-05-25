b9b35238c03e1bd28e90ffa07b38a197c2a0ec74
Vit Machalek <vit.mach61@gmail.com>
2021-05-25 16:25:56 +0200

    style(contacts) change em to rem, add top margin changed me to rem in margins and paddings, add top margin component

--- components/Form/styled.ts
+++ components/Form/styled.ts
@@ -15,6 +15,7 @@ flex-wrap: wrap;
 width: 100%;
 color: ${({theme}) => theme.colors.yellow};
 font-family: sourceCodeVariable;
+margin: 0 0 2rem 0;
 `
 
 export const FormTextArea = styled.textarea`
@@ -24,22 +25,25 @@ font-size: 1em;
 background-color: ${({theme}) => theme.colors.black};
 border: none;
 color: ${({theme}) => theme.colors.white};
-margin: .4em 0 .8em 0;
-padding: .4em;
+padding: 1rem;
+margin: 1rem 0 0 0;
+resize: none;
 `
 
-export const FormInput = styled.input`
+export const FormInput = styled.input<{isInValid?: boolean}>`
 background-color: ${({theme}) => theme.colors.black};
-border: none;
+border: solid 2px ${({isInValid}) => (isInValid)? "red" : "transparent" };
 color: ${({theme}) => theme.colors.white};
 font-size: 1em;
-padding: .2em .4em;
-margin: 0 0 0 .8em;
+padding: .5rem 1rem;
+margin: 0 1.6rem 0 1.6rem;
+flex-grow:1;
 `
 
 export const FormSubmit = styled.input`
 background-color: ${({theme}) => theme.colors.yellow};
-padding: .2em 1em;
+padding: .6rem 7rem;
+margin: 0 0 2rem 0;
 font-size: 1em;
 border: none;
 `
@@ -53,4 +57,8 @@ width: 100%;
 @media (min-width: ${({ theme }) => theme.mediaQueries.ipad}) {
     flex-direction: row;
 }
+`
+
+export const TopMargin = styled.div`
+    margin-top: 10%;
 `
