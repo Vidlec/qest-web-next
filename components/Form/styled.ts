9693fa101fba7cf4e93e07c9fb721818fc9ba235
Vit Machalek <vit.mach61@gmail.com>
2021-05-28 09:54:04 +0200

    refactor(form) refactor of components/styled.ts remove em units, white spaces, unnecessary Label and unnecessary code

--- components/Form/styled.ts
+++ components/Form/styled.ts
@@ -6,70 +6,63 @@ export const FormWrapper = styled.form`
 	width: 100%;
 	justify-content: center;
 	position: relative;
-	font-size: 0.9em;
-	margin-top: 30px;
-	font-size: 20px;
+	font-size: 1.4rem;
+	padding-top: 30px;
 	font-family: sourceCodeVariable;
+	color: ${({ theme }) => theme.colors.yellow};
 `
 
-export const FormLabelText = styled.label`
-	display: flex;
-	flex-wrap: wrap;
-	align-items: center;
-	width: 100%;
-	margin: 1rem 0 1rem 0;
-`
-
-export const FormLabelInput = styled.label`
-	display: flex;
-	flex-wrap: nowrap;
-	align-items: center;
-	justify-content: space-between;
-	width: 100%;	
-	padding: 0 0 1.6rem 0;
-	margin: 1rem 0 1rem 0;
+export const FormLabel = styled.label`
 	position: relative;
+	display: flex;
+	align-items: baseline;
+	justify-content: space-between;
+	width: 100%;
+
+	@media (min-width: ${({ theme }) => theme.mediaQueries.desktopSmall}) {
+		justify-content: flex-start;
+	}
 `
 
-export const FormLabelHeading = styled.span`
-	font-size: .9em;
-	color: ${({theme}) => theme.colors.yellow};
+export const FormLabelSpan = styled.span`
+	margin: 0 0.8rem 0.8rem 0;
 `
 
 export const FormTextArea = styled.textarea`
 	width: 100%;
 	height: 256px;
-	font-size: 1em;
+	font-size: 1.4rem;
 	background-color: ${({ theme }) => theme.colors.black};
-	border: none;
 	color: ${({ theme }) => theme.colors.white};
+	border: none;
 	padding: 1rem;
-	margin: 1rem 0 0 0;
+	margin: 0 0 2.6rem 0;
 	resize: none;
+
+	@media (min-width: ${({ theme }) => theme.mediaQueries.desktopSmall}) {
+		margin: 0 0 1.6rem 0;
+	}
 `
 
 export const FormInput = styled.input<{ isValid?: boolean }>`
-	background-color: ${({ theme }) => theme.colors.black};
-	border: solid 2px ${({ isValid, theme }) => (isValid === false ? theme.colors.red : 'transparent')};
-	outline-color: 2px solid ${({isValid, theme}) => (isValid === false ? theme.colors.red: 'transparent')};
-	color: ${({ theme }) => theme.colors.white};
-	font-size: 1em;
-	padding: 0.5rem 1rem;
-	margin: 0 0 0 1.6rem;
 	width: 80%;
-	flex-shrink: 1;
-
+	font-size: 1.4rem;
+	background-color: ${({ theme }) => theme.colors.black};
+	color: ${({ theme }) => theme.colors.white};
+	border: solid 2px ${({ isValid, theme }) => (isValid ? 'transparent' : theme.colors.red)};
+	padding: 0.5rem 1rem;
+	margin: 0 0 2.6rem 0;
+	
 	@media (min-width: ${({ theme }) => theme.mediaQueries.desktopSmall}) {
-		margin: 0 1.6rem 0 1.6rem;
-		widht: 100%;
+		margin: 0 2rem 2.6rem 0;
 	}
 `
 
 export const FormSubmit = styled.input`
 	background-color: ${({ theme }) => theme.colors.yellow};
 	padding: 0.6rem 7rem;
-	margin: 1rem 0 2.6rem 0;
-	font-size: 1em;
+	margin: 0 0 2.6rem 0;
+	font-size: 1.4rem;
 	border: none;
 `
 
@@ -83,17 +76,10 @@ export const FormRow = styled.div`
 		flex-direction: row;
 	}
 `
-
-export const TopMargin = styled.div`
-	margin-top: 10%;
-`
-
 export const WarningMessage = styled.div`
 	position: absolute;
-	bottom: 0;
+	bottom: 0.3rem;
 	left: 0;
-
 	font-size: 1.2rem;
-
-	color: ${({theme}) => (theme.colors.red)}
+	color: ${({ theme }) => theme.colors.red};
 `
