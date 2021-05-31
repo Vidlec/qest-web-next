71e168b7fd58c295c18e28b3a0f6e5b8b82cc7b7
Vit Machalek <vit.mach61@gmail.com>
2021-05-31 21:31:56 +0200

    refactor(PictureImg) created Component PictureImg recreate PictureImg from About page

---
+++ components/PictureImg/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Img } from './styled'
+
+interface PictreImgProps {
+	src: string
+	alt: string
+}
+
+const PictureImg: React.FC<PictreImgProps> = ({ src, alt }) => {
+	return <Img src={src} alt={alt} />
+}
+
+export default PictureImg
