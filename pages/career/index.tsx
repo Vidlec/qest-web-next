6264a24a41b95a8e93f4cbecac2776726654ac18
Vit Machalek <vit.mach61@gmail.com>
2021-05-31 21:34:10 +0200

    refactor(career) refactor Components

--- pages/career/index.tsx
+++ pages/career/index.tsx
@@ -3,16 +3,10 @@ import React from 'react'
 import Container from 'components/Container'
 import Headline from 'components/Headline'
 import Col from 'components/Col'
+import Row from 'components/Row'
 import SquareList from 'components/SquareList'
-import {
-	CareerDescription,
-	CareerCTA,
-	TechnologiesCard,
-	TechnologiesPicture,
-	TechnologiesDescription,
-	PictureImg,
-	SkillsRow,
-} from 'components/About/styled'
+import TechnologiesCard from 'components/TechnologiesCard'
+import theme from 'theme'
 import { useTranslation } from 'react-i18next'
 
 const squareData: { header: string, description: string }[] = [
@@ -25,42 +19,44 @@ const squareData: { header: string, description: string }[] = [
 const Career: React.FC = () => {
 	const { t } = useTranslation()
 	return (
-		<Container>
-			<SkillsRow>
-				<Col mobile={12} desktopSmall={7}>
-					<Headline>{t('about.careerHeadline')}</Headline>
+		<>
+            <Container>
+                <br></br>
+                <br></br>
+                <br></br>
+                <br></br>
+                <Row>
+                    <Col mobile={12} desktopSmall={7}>
+                        <Headline color={theme.colors.green}>{t('about.careerHeadline')}</Headline>
+                        <div
+                            dangerouslySetInnerHTML={{
+                                __html: t('about.careerDescription'),
+                            }}
+                        />
+                    </Col>
+                    <Col>
+                        <TechnologiesCard
+                            src={t('about.technologiesImage.url')}
+                            alt={t('about.technologiesImage.alternativeText')}
+                            __html={t('about.technologiesDescription')}
+                        />
+                    </Col>
+                </Row>
+            </Container>
+            <Container>
+                <Headline>Koho Hledáme</Headline>
+                <SquareList squares={squareData}>
+                    <h5>Umíš něco jiného?</h5>
+                    <p>Nevadí. Napiš a třeba se domluvíme.</p>
+                    <a href="#" >Napiš nám</a>
+                </SquareList>
 
-					<CareerDescription
-						dangerouslySetInnerHTML={{
-							__html: t('about.careerDescription'),
-						}}
-					/>
-				</Col>
-				<Col>
-					<TechnologiesCard>
-						<TechnologiesPicture>
-							<PictureImg
-								src={t('about.technologiesImage.url')}
-								alt={t(
-									'about.technologiesImage.alternativeText'
-								)}
-							/>
-						</TechnologiesPicture>
-						<TechnologiesDescription
-							dangerouslySetInnerHTML={{
-								__html: t('about.technologiesDescription'),
-							}}
-						/>
-					</TechnologiesCard>
-				</Col>
-			</SkillsRow>
-			<Headline>Koho Hledáme</Headline>
-			<SquareList squares={squareData}>
-                <h5>Umíš něco jiného?</h5>
-                <p>Nevadí. Napiš a třeba se domluvíme.</p>
-                <a href="#" >Napiš nám</a>
-            </SquareList>
-		</Container>
+                <p>Líbí se ti naše práce? kounki na více</p>
+            </Container>
+            <Container>
+                <Headline>Qest to jsou zkušenosti, schopnosti a vášeň</Headline>
+            </Container>
+        </>
 	)
 }
 
