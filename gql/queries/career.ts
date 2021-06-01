9304f35d3935a1bdd845cfb0a9af9d294aedeab4
Vit Machalek <vit.mach61@gmail.com>
2021-06-01 15:54:59 +0200

    feat(career) Add translations

---
+++ gql/queries/career.ts
@@ -0,0 +1,17 @@
+import { gql } from 'apollo-boost'
+
+export const CAREER_QUERY = gql`
+	query {
+		careers {
+			id
+            language
+            title
+            technologiesHeading
+            technologies
+            careerDescription
+            somethingElseHeading
+            somethingElseDescription
+            somethingElseContact
+		}
+	}
+`
