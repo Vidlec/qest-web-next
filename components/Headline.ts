57d2af624d83b576fd31220bf93c7314eaae3366
Vit Machalek <vit.mach61@gmail.com>
2021-06-25 15:20:05 +0200

    style Add sizing to theme

--- components/Headline.ts
+++ components/Headline.ts
@@ -2,16 +2,15 @@ import styled from 'styled-components'
 
 const Headline = styled.h1<{color?: string}>`
 	color: ${({ color, theme }) =>  color? color : theme.colors.lightGray};
-	font-size: 2rem;
+	font-size: ${({theme}) => theme.sizes.large};
 	font-weight: bold;
 	letter-spacing: 0;
 	line-height: 1;
 	margin-top: 0;
-	margin-bottom: 1.7rem;
+	margin-bottom: 3.6rem;
 
-	@media (min-width: ${({ theme }) => theme.mediaQueries.mobileLarge}) {
-		font-size: 4.4rem;
-		margin-bottom: 3.6rem;
+	@media (min-width: ${({ theme }) => theme.mediaQueries.desktop}) {
+		font-size: ${({theme}) => theme.sizes.pageTitle}
 	}
 `
 
