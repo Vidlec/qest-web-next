d24e0190062514f1142e2fe7b3edb5a4d5b019cc
Vit Machalek <vit.mach61@gmail.com>
2021-06-25 13:10:32 +0200

    feat(Career) use getStaticProps Use getStaticProps, graphql-request Use new directory composition

--- pages/career/index.tsx
+++ pages/career/index.tsx
@@ -1,3 +1,15 @@
-import CareerPage from "screens/career"
+import request from 'graphql-request'
+import CareerPage, { Props as CareerQuery } from 'screens/career'
+import { CAREER_QUERY } from 'gql/queries/career'
+import { GetStaticProps } from 'next'
 
 export default CareerPage
+
+export const getStaticProps: GetStaticProps = async (context) => {
+	const props = await request<CareerQuery>(
+		process.env.CMS_GRAPHQL_URL!,
+		CAREER_QUERY
+	)
+
+	return { props }
+}
--- screens/career/index.tsx
+++ screens/career/index.tsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import request from 'graphql-request'
 import Container from 'components/Container'
 import Headline from 'components/Headline'
 import Col from 'components/Col'
@@ -8,104 +7,80 @@ import SquareList, { EndHead, ContactUs } from 'components/SquareList'
 import TechnologiesCard from 'components/TechnologiesCard'
 import PictureList from 'components/PictureList'
 import Paragraph from 'components/Paragraph'
-import theme from 'theme'
 import { useTranslation } from 'react-i18next'
 import {
 	Career,
 	ComponentContentPictureList,
 	UploadFile,
-    CareerOfferings
+	CareerOfferings,
 } from 'gql/generated/types'
-import {
-    CAREER_QUERY
-} from 'gql/queries/career'
-import { useEffect } from 'react'
+import { useTheme } from 'styled-components'
 
-interface CareerQuery {
+export interface Props {
 	career: Career
 }
 
-const CareerPage: React.FC<CareerQuery> = ({ career }) => {
+const CareerPage: React.FC<Props> = ({ career }) => {
 	const { t } = useTranslation()
-
-    ///console.log(career)
 
 	const careerPositions: CareerOfferings[] = []
 	const pictureListData: ComponentContentPictureList[] = []
 
-    useEffect(() => {
+	career.info?.map((item) => {
+		item && pictureListData.push(item)
+	})
 
-    }, [])
-/*
-	if( data ) {
-		const { career } = data
+	career.careerPositions?.map((position) => {
+		position && careerPositions.push(position)
+	})
 
-		console.log(career.info?.map(item => item))
+	return (
+		<>
+			<Container>
+				<Row>
+					<Col mobile={12} desktopSmall={7} desktop={8}>
+						<Headline color={useTheme().colors.green}>
+							{career.title}
+						</Headline>
+						<Paragraph
+							dangerouslySetInnerHTML={{
+								__html: career.description,
+							}}
+						/>
+					</Col>
+					<Col>
+						<TechnologiesCard
+							hand={t<string, UploadFile>(
+								'about.technologiesImage',
+								{ returnObjects: true }
+							)}
+							technologies={career.technologies?.technologies}
+							description={career.technologies?.description}
+							images={career.technologies?.images}
+							id={''}
+							handIsOnMiddle
+						/>
+					</Col>
+				</Row>
+				<Headline>{career.careerWho}</Headline>
+				<SquareList squares={careerPositions}>
+					<EndHead>{career.somethingElseHeading}</EndHead>
+					<p>{career.somethingElseDescription}</p>
+					<ContactUs href="#">
+						{career.somethingElseContact}
+					</ContactUs>
+				</SquareList>
 
-		career.info?.map(item => {
-			item && pictureListData.push(item)
-		})
-
-		career.careerPositions?.map(position => {
-			console.log(position)
-			position && careerPositions.push(position)
-			console.log(careerPositions)
-		})
-
-		return (
-			<>
-				<Container>
-					<Row>
-						<Col mobile={12} desktopSmall={7} desktop={8}>
-							<Headline color={theme.colors.green}>
-								{career.title}
-							</Headline>
-							<Paragraph
-								dangerouslySetInnerHTML={{
-									__html: career.description,
-								}}
-							/>
-						</Col>
-						<Col>
-							<TechnologiesCard
-								hand={t<string, UploadFile>('about.technologiesImage', {returnObjects: true})}
-								technologies={career.technologies?.technologies}
-								description={career.technologies?.description}
-								images={career.technologies?.images}
-								id={''}
-								handIsOnMiddle
-							/>
-						</Col>
-					</Row>
-					<Headline>{career.careerWho}</Headline>
-					<SquareList squares={careerPositions}>
-						<EndHead>{career.somethingElseHeading}</EndHead>
-						<p>{career.somethingElseDescription}</p>
-						<ContactUs href="#">{career.somethingElseContact}</ContactUs>
-					</SquareList>
-	
-					<Paragraph
-						dangerouslySetInnerHTML={{ __html: career.careerWhy as string}}
-					/>
-					<Headline>{career.careerWhatHeading}</Headline>
-					<PictureList items={pictureListData} />
-				</Container>
-			</>
-		)
-	}	
-*/
-	return null
-}
-
-export const getStaticProps = async (context) => {
-
-	console.log("text", process.env.CMS_GRAPHQL_URL!)
-
-	const data = await request<CareerQuery>(process.env.CMS_GRAPHQL_URL!, CAREER_QUERY)
-
-	console.log( data )
-
-	return { props: { career: data.career } }
+				<Paragraph
+					dangerouslySetInnerHTML={{
+						__html: career.careerWhy as string,
+					}}
+				/>
+				<Headline>{career.careerWhatHeading}</Headline>
+				<PictureList items={pictureListData} />
+			</Container>
+		</>
+	)
 }
 
 export default CareerPage
