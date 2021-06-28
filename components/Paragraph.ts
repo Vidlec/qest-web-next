2ef03c36d9bb58a6ef76ade546727085f673fa06
Vit Machalek <vit.mach61@gmail.com>
2021-06-28 12:59:17 +0200

    refactor(Paragraph) remove a styling

--- components/Paragraph.ts
+++ components/Paragraph.ts
@@ -4,10 +4,6 @@ const Paragraph = styled.p`
     font-size: ${({ theme }) => theme.sizes.extraSmall};
     font-size: 1.3rem;
     margin: 0 0 1rem 0;
-  
-    a {
-        text-decoration: underline;
-    }
 
     @media (min-width: ${({ theme }) => theme.mediaQueries.desktop}) {
       font-size: ${({ theme }) => theme.sizes.body};
