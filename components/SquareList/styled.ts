3c5a6336bc576a33855f8abb1841b1f8f777e797
Vit Machalek <vit.mach61@gmail.com>
2021-06-28 13:01:38 +0200

    style(SquareList) change font size to theme

--- components/SquareList/styled.ts
+++ components/SquareList/styled.ts
@@ -76,7 +76,7 @@ export const End = styled.div`
 
 export const EndHead = styled.h5`
 	margin: 0 0 3rem 0;
-	font-size: ${({theme}) => theme.sizes.body};
+	font-size: ${({theme}) => theme.sizes.headingSmall};
 	color: ${({ theme }) => theme.colors.lightGray};
 `
 
