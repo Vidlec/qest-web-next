d494046c11e4bc8d45d0fb0719638e0b88560e31
Vit Machalek <vit.mach61@gmail.com>
2021-06-30 09:46:14 +0200

    refactor(Reference) fix props type

--- pages/reference/index.tsx
+++ pages/reference/index.tsx
@@ -1,3 +1,16 @@
 import Reference from 'screens/Reference'
+import { request } from 'graphql-request'
+import { GetStaticProps } from 'next'
+import { CASE_STUDY_QUERY } from 'gql/queries/caseStudy'
+import { CaseStudy } from 'gql/generated/types'
 
 export default Reference
+
+export const getStaticProps: GetStaticProps = async (context) => {
+	const props = await request<CaseStudy>(
+		process.env.CMS_GRAPHQL_URL!,
+		CASE_STUDY_QUERY
+	)
+
+	return { props }
+}
--- screens/Reference/index.tsx
+++ screens/Reference/index.tsx
@@ -1,13 +1,19 @@
 import React from 'react'
+import { CaseStudy } from 'gql/generated/types'
 import Content from './Content'
 import Header from 'components/Header'
 import Footer from 'components/Footer'
 
-const Reference: React.FC = () => {
+export interface Props {
+	caseStudies: CaseStudy[]
+}
+
+const Reference: React.FC<Props> = ({caseStudies}) => {
+
 	return (
 		<>
 			<Header />
-			<Content />
+			<Content caseStudies={caseStudies} />
 			<Footer />
 		</>
 	)
