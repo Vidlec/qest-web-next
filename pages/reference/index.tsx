4603570102cea55f38d98e3a8c94b45b5a65cfe5
Vit Machalek <vit.mach61@gmail.com>
2021-06-30 17:05:43 +0200

    update request generic type

--- pages/reference/index.tsx
+++ pages/reference/index.tsx
@@ -2,12 +2,12 @@ import Reference from 'screens/Reference'
 import { request } from 'graphql-request'
 import { GetStaticProps } from 'next'
 import { CASE_STUDIES_QUERY } from 'gql/queries/caseStudies'
-import { CaseStudy } from 'gql/generated/types'
+import { CaseStudiesQuery } from 'gql/generated/types'
 
 export default Reference
 
 export const getStaticProps: GetStaticProps = async (context) => {
-	const props = await request<CaseStudy>(
+	const props = await request<CaseStudiesQuery>(
 		process.env.CMS_GRAPHQL_URL!,
 		CASE_STUDIES_QUERY
 	)
