74021cb7a6d11d86a080b7898355a5096fa1881a
Vit Machalek <vit.mach61@gmail.com>
2021-06-30 09:46:56 +0200

    feat(reference) add simplier gallery

--- screens/Reference/Content/index.tsx
+++ screens/Reference/Content/index.tsx
@@ -1,69 +1,47 @@
 import React from 'react'
+import { CaseStudy } from 'gql/generated/types'
 import Container from 'components/Container'
 import { ReferenceParagraph } from './ReferenceParagraph'
 import {
 	Wrapper,
-	GalleryItemCube,
-	GalleryItemStand,
-	GalleryItemLay,
-	WhiteSpace,
 	Image,
+	Item,
 } from './styled'
 import Line from 'components/Line'
 import { useTranslation } from 'react-i18next'
 
-const Content: React.FC = () => {
+export interface Props {
+	caseStudies: CaseStudy[]
+}
+
+const Content: React.FC<Props> = ({caseStudies}) => {
 	const { t } = useTranslation()
 
-	const projects: React.ReactElement[] = []
-	const first = ['', '', '', '', '', '', '', '', '']
-	const second = ['']
-
-	console.log(Math.ceil(first.length / 3), Math.ceil(second.length / 3))
+	const list = caseStudies.map((caseStudy) => caseStudy.title as string)
+	caseStudies.map((caseStudy) => list.push(caseStudy.title as string))
 
 	return (
 		<Container>
-			<ReferenceParagraph>
-				Our clients include {projects}
+			<ReferenceParagraph items={list}>
+				Our clients include{' '}
 			</ReferenceParagraph>
 			<Line></Line>
-			<Wrapper rows={4}>
-				<GalleryItemCube>
-					<Image src={'https://dummyimage.com/400x400/000/fff'} />
-				</GalleryItemCube>
-				<GalleryItemStand>
-					<Image src={'https://dummyimage.com/400x600/000/fff'} />
-				</GalleryItemStand>
-				<GalleryItemLay>
-					<Image src={'https://dummyimage.com/600x400/000/fff'} />
-				</GalleryItemLay>
-				<GalleryItemCube>
-					<Image src={'https://dummyimage.com/400x400/000/fff'} />
-				</GalleryItemCube>
-				<GalleryItemStand>
-					<Image src={'https://dummyimage.com/400x600/000/fff'} />
-				</GalleryItemStand>
-				<GalleryItemLay>
-					<Image src={'https://dummyimage.com/600x400/000/fff'} />
-				</GalleryItemLay>
-				<GalleryItemCube>
-					<Image src={'https://dummyimage.com/400x400/000/fff'} />
-				</GalleryItemCube>
-				<GalleryItemStand>
-					<Image src={'https://dummyimage.com/400x600/000/fff'} />
-				</GalleryItemStand>
-				<GalleryItemLay>
-					<Image src={'https://dummyimage.com/600x400/000/fff'} />
-				</GalleryItemLay>
-				<GalleryItemCube>
-					<Image src={'https://dummyimage.com/400x400/000/fff'} />
-				</GalleryItemCube>
-				<GalleryItemStand>
-					<Image src={'https://dummyimage.com/400x600/000/fff'} />
-				</GalleryItemStand>
-				<GalleryItemLay>
-					<Image src={'https://dummyimage.com/600x400/000/fff'} />
-				</GalleryItemLay>
+			<Wrapper>
+				{caseStudies.map((caseStudy) => {
+					return (
+						<Item key={caseStudy?.id} height={caseStudy?.height as number}>
+							<Image src={caseStudy?.image?.url} />
+						</Item>
+					)
+				})}
+
+				{caseStudies.map((caseStudy) => {
+					return (
+						<Item key={caseStudy?.image?.url} height={caseStudy?.height as number}>
+							<Image src={caseStudy?.image?.url} />
+						</Item>
+					)
+				})}
 			</Wrapper>
 		</Container>
 	)
--- screens/Reference/Content/styled.ts
+++ screens/Reference/Content/styled.ts
@@ -1,113 +1,23 @@
 import styled from 'styled-components'
 
-
-
-export const Wrapper = styled.div<{rows: number}>`
-	top: 0;
-	left: 0;
+export const Wrapper = styled.div`
+	position: relative;
 	width: 100%;
-	height: 100%;
+	min-height: 200vh;
 	display: grid;
-	grid-template-columns: repeat(9, 1fr);
-	grid-template-rows: repeat(9, 1fr);
-	grid-auto-flow: row dense;
-	gap: 60px;
-
-	justify-items: center;
-	justify-content: center;
-	align-items: flex-end;
-
-	@media ( min-width: ${({theme}) => theme.mediaQueries.desktop }) {
-		grid-template-columns: repeat(13, 1fr);
-		grid-template-rows: repeat(13, 1fr);
-		gap: 30px;
-	}
+	grid-template-columns: repeat(3, 1fr);
+	grid-auto-rows: auto;
+	gap: 2rem;
 `
 
-
-export const GalleryItemCube = styled.div`
-	width: 100%;
-	height: 100%;
-	grid-column-end: span 4;
-	grid-row-end: span 4;
-	background-color: #0ff;
-`
-
-export const GalleryItemStand = styled.div`
-	width: 100%;
-	height: 100%;
-	grid-column-end: span 4;
-	grid-row-end: span 5;
-	background-color: #f0f;
-`
-
-export const GalleryItemLay = styled.div`
-	width: 100%;
-	height: 100%;
-	grid-column-end: span 5;
-	grid-row-end: span 4;
-	background-color: #ff0;
-`
-
-export const WhiteSpace = styled.div`
-	width: 100%;
-	height: 100%;
-	grid-column-end: span 1;
-	grid-row-end: span 1;
+export const Item = styled.div<{ height: number }>`
+	grid-row-start: span ${({ height }) => height};
 `
 
 export const Image = styled.img`
+	position: relative;
 	width: 100%;
 	height: 100%;
 	object-fit: cover;
 	object-position: center;
 `
-
-/*
-export const GalleryWrapper = styled.div<{rows: number}>`
-	position: relative;
-	width: 100%;
-	padding-top: ${({rows}) => rows * 33.333}%;
-	font-size: 2rem;
-	margin-bottom: 4%;
-`
-
-export const GridWrapper = styled.div<{rows: number}>`
-	position: absolute;
-	top: 0;
-	left: 0;
-	width: 100%;
-	height: 100%;
-	display: grid;
-	gap: ${({rows}) => 20 - 4 * ( rows + 1 )}% 4%;
-	grid-template-columns: repeat(13, 4%);
-	grid-template-rows: repeat( ${({rows}) => rows * 4 + 1}, ${({rows}) => 20 - 4 * ( rows + 1 )}%);
-`
-
-export const GalleryItemCube = styled.div`
-	grid-column-end: span 4;
-	grid-row-end: span 4;
-    background-color: #0ff;
-`
-
-export const GalleryItemStand = styled.div`
-	grid-column-end: span 4;
-	grid-row-end: span 5;
-    background-color: #f0f;
-`
-
-export const GalleryItemLay = styled.div`
-	grid-column-end: span 5;
-	grid-row-end: span 4;
-    background-color: #ff0;
-`
-
-export const WhiteSpace = styled.div`
-	grid-column-end: span 1;
-	grid-row-end: span 1;
-`
-
-export const DotSpan = styled.span<{ color: string }>`
-	color: ${({color}) => (color)};
-	margin: 0 1rem;
-`*/
